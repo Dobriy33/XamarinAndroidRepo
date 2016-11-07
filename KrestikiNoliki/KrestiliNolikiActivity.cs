55eb74820c5ce3ce240e75bcef647df6fa06066f
Pavel T. <TepluhinPA@yandex.ru>
2016-11-07 13:27:47 +0300

    Initialization Game in KrestikiNolikiActivity

--- KrestikiNoliki/KrestiliNolikiActivity.cs
+++ KrestikiNoliki/KrestiliNolikiActivity.cs
@@ -9,20 +9,28 @@ using Android.OS;
 namespace KrestikiNoliki
 {
     [Activity(Label = "KrestikiNoliki", MainLauncher = true, Icon = "@drawable/icon")]
-    public class MainActivity : Activity
+    public class KrestiliNolikiActivity : Activity
     {
         int count = 1;
         private TableLayout layout;
+        public KrestikinolikiActivity()
+        {
+            game = new Game();
+            game.start(); // будет реализован позже
+        }
+
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.TableLayout);
-            TableLayout layout = (TableLayout) FindViewById(Resource.Id.main_l);
+            TableLayout layout = (TableLayout)FindViewById(Resource.Id.main_l);
             // Get our button from the layout resource,
             // and attach an event to it
             buildGameField();
+
         }
     }
 }
