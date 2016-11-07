534150f17037514140fe1d2021cc2903d4e02412
Pavel T. <TepluhinPA@yandex.ru>
2016-11-07 13:18:12 +0300

    Class Game added

---
+++ KrestikiNoliki/Game.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KrestikiNoliki
+{
+    public class Game
+    {
+        //поле
+        private Square[,] field;
+
+        public Game()
+        {
+            field = new Square[3,3];
+            int squareCount = 0;
+            for (int i = 0, l = field.Length; i < l; i++)
+            {
+                for (int j = 0, l2 = field.Length; j < l2; j++)
+                {
+                    field[i,j] = new Square();
+                    squareCount++;
+                }
+            }
+        }
+
+        public Square[,] getField()
+        {
+            return field;
+        }
+
+        }
+}
