f476d3c2d58659e918bfe07735d83bbb997c8294
Pavel T. <TepluhinPA@yandex.ru>
2016-11-17 14:27:36 +0300

    add diagonalRight win checker

---
+++ KrestikiNoliki/WinnerCheckerDiagonalRight.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace KrestikiNoliki
+{
+    class WinnerCheckerDiagonalRight
+    {
+        private Game game;
+
+        public WinnerCheckerDiagonalRight(Game game)
+        {
+            this.game = game;
+        }
+
+        public Player checkWinner()
+        {
+            Square[,] field = game.getField();
+            Player currPlayer;
+            Player lastPlayer = null;
+            int successCounter = 1;
+            for (int i = 0, len = field.GetLength(0); i < len; i++)
+            {
+                currPlayer = field[i,len - (i + 1)].getPlayer();
+                if (currPlayer != null)
+                {
+                    if (lastPlayer == currPlayer)
+                    {
+                        successCounter++;
+                        if (successCounter == len)
+                        {
+                            return currPlayer;
+                        }
+                    }
+                }
+                lastPlayer = currPlayer;
+            }
+            return null;
+        }
+    }
+}
